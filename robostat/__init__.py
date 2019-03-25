d44c034313df10b27810b86a2eb0587696d33f41
tl <tl@cat.pm>
2019-03-26 01:17:43 +0200

    robostat: replace_default_tournament lisätty

--- robostat/__init__.py
+++ robostat/__init__.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from pkgutil import extend_path
 from .tournament import Tournament
 
@@ -6,6 +7,16 @@ from .tournament import Tournament
 __path__ = extend_path(__path__, __name__)
 
 default_tournament = Tournament()
+
+@contextmanager
+def replace_default_tournament(tournament):
+    global default_tournament
+    old_default = default_tournament
+    default_tournament = tournament
+    try:
+        yield
+    finally:
+        default_tournament = old_default
 
 block = lambda *args, **kwargs: default_tournament.block(*args, **kwargs)
 ranking = lambda *args, **kwargs: default_tournament.ranking(*args, **kwargs)
